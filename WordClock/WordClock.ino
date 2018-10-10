794aa806eaaeb9ebb2ac3cebb40ba48732026c7e
Alexandros Dimitriadis <alathdim@gmail.com>
2018-10-11 01:27:33 +0300

    showTimeText function added
    minutes variables and show function added

--- WordClock/WordClock.ino
+++ WordClock/WordClock.ino
@@ -19,9 +19,17 @@ int Eleven[] = {31,35};
 int Twelve[] = {44,49};
 int Kai[] = {60,62};
 int Para[] = {65,68};
+int Pente[] = {95,99};
+int Deka[] = {76,79};
+int Tetarto[] = {81,87};
+int Eikosi[] = {70,75};
+int Misi[] = {90,93};
+
 uint32_t c =  strip.Color(20, 100, 250);
-int Hour = 00;
-int Min = 00;
+int Hour = 0;
+int Min = 0;
+int HourWord[2], ConnectWord[2], MinuteWord[2];
+
 void setup() {
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -32,53 +40,64 @@ void setup() {
 
 void loop() {
   clearStrip();
-  if (Hour == 0 | Hour == 12){
-    showNum (Twelve, c);
-    if (Hour == 0) Hour= 0;
+ 
+  if (Hour == 0 | Hour == 12){ HourWord[0] = Twelve[0]; HourWord[1] = Twelve[1];}
+  else if (Hour == 1 | Hour ==13) { HourWord[0] = One[0]; HourWord[1] = One[1]; }
+  else if (Hour == 2 | Hour ==14) { HourWord[0] = Two[0]; HourWord[1] = Two[1]; }
+  else if (Hour == 3 | Hour ==15) { HourWord[0] = Three[0]; HourWord[1] = Three[1]; }
+  else if (Hour == 4 | Hour ==16) { HourWord[0] = Four[0]; HourWord[1] = Four[1]; }
+  else if (Hour == 5 | Hour ==17) { HourWord[0] = Five[0]; HourWord[1] = Five[1]; }
+  else if (Hour == 6 | Hour ==18) { HourWord[0] = Six[0]; HourWord[1] = Six[1]; } 
+  else if (Hour == 7 | Hour ==19) { HourWord[0] = Seven[0]; HourWord[1] = Seven[1]; }
+  else if (Hour == 8 | Hour ==20) { HourWord[0] = Eight[0]; HourWord[1] = Eight[1]; }
+  else if (Hour == 9 | Hour ==21) { HourWord[0] = Nine[0]; HourWord[1] = Nine[1]; }
+  else if (Hour == 10 | Hour == 22) { HourWord[0] = Ten[0]; HourWord[1] = Ten[1]; }
+  else {
+      HourWord[0] = Eleven[0]; HourWord[1] = Eleven[1];
+      if (Hour == 23) Hour = 0;}
+  
+  if (Min <= 30) { 
+    ConnectWord[0] = Kai[0]; ConnectWord[1] = Kai[1];
+    if (Min <5) { MinuteWord[0] = 0; MinuteWord[1] = 0; }
+    else if (Min <10) { MinuteWord[0] = Pente[0]; MinuteWord[1] = Pente[1]; }
+    else if (Min <15) { MinuteWord[0] = Deka[0]; MinuteWord[1] = Deka[1]; }
+    else if (Min <20) { MinuteWord[0] = Tetarto[0]; MinuteWord[1] = Tetarto[1]; }
+    else if (Min <25) { MinuteWord[0] = Eikosi[0]; MinuteWord[1] = Eikosi[1]; }
+    else { MinuteWord[0] = Misi[0]; MinuteWord[1] = Misi[1]; }
     }
-  if (Hour == 1 | Hour ==13) showNum (One, c);
-  if (Hour == 2 | Hour ==14) showNum (Two, c);
-  if (Hour == 3 | Hour ==15) showNum (Three, c);
-  if (Hour == 4 | Hour ==16) showNum (Four, c);
-  if (Hour == 5 | Hour ==17) showNum (Five, c);
-  if (Hour == 6 | Hour ==18) showNum (Six, c);
-  if (Hour == 7 | Hour ==19) showNum (Seven, c);
-  if (Hour == 8 | Hour ==20) showNum (Eight, c);
-  if (Hour == 9 | Hour ==21) showNum (Nine, c);
-  if (Hour == 10 | Hour == 22) showNum (Ten, c);
-  if (Hour == 11 | Hour == 23) showNum (Eleven, c);
-  if (Min <= 30) showNum(Kai, c);
-  else showNum (Para,c);
+  
+  else { 
+    ConnectWord[0] = Para[0]; ConnectWord[1] = Para[1];
+    if (Min <5) { MinuteWord[0] = 0; MinuteWord[1] = 0; }
+    else if (Min >50) { MinuteWord[0] = Pente[0]; MinuteWord[1] = Pente[1]; }
+    else if (Min >45) { MinuteWord[0] = Deka[0]; MinuteWord[1] = Deka[1]; }
+    else if (Min >40) { MinuteWord[0] = Tetarto[0]; MinuteWord[1] = Tetarto[1]; }
+    else if (Min >35) { MinuteWord[0] = Eikosi[0]; MinuteWord[1] = Eikosi[1]; }
+    }
+ 
+  showTimeText (HourWord, ConnectWord, MinuteWord, c);
   delay(2000);
   Hour ++;
   Min += 5;
 }
 
-
-void showNum(int Num[], uint32_t c){
-  for(int i=Num[0]; i<=Num[1]; i++) {
-      strip.setPixelColor(i,c);
-      strip.show();
+void showTimeText(int HourWord[],int ConnectWord[],int MinuteWord[], uint32_t c){
+  for(int i=HourWord[0]; i<=HourWord[1]; i++) {
+      strip.setPixelColor(i,c);      
   }
+  for(int i=ConnectWord[0]; i<=ConnectWord[1]; i++) {
+      strip.setPixelColor(i,c);      
+  }
+  for(int i=MinuteWord[0]; i<=MinuteWord[1]; i++) {
+      strip.setPixelColor(i,c);      
+  }
+  strip.show();
 }
 
 void clearStrip(){
   for(uint16_t i=0; i<100; i++) {
       strip.setPixelColor(i, 0);
-      strip.show();
   }
+  strip.show();
 
-//void showSix(){
-//  for(int i=57; i<=59; i++) {
-//      strip.setPixelColor(i, strip.Color(200, 100, 230));
-//      strip.show();
-//  }
-//}
-//
-//void showNine(){
-//  for(uint16_t i=50; i<=54; i++) {
-//      strip.setPixelColor(i, strip.Color(100, 200, 130));
-//      strip.show();
-//  }
-//}
 }
